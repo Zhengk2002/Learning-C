aee607d18b603dac7e7b401241d32e6f0ae4e128
Zhengk2002 <135444549+Zhengk2002@users.noreply.github.com>
2023-06-11 12:29:16 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Today is Saturday.\n");
+    printf("Tomorrow is Sunday.\n"); /*These two lines are for practicing printf function*/
+
+    int a;
+    a = 20;
+    int b;
+    b = 5;
+    printf("The value for a currently is: %d\n", a);
+    printf("The value for b currently is: %d\n", b);
+    printf("Currently, the value for a and b are: %d, %d\n", a, b); /*These three lines are for practicing printf function as well */
+
+    b = -5;
+    printf("The value for b is: %d\n", b);  /* learn what is variable,*/
+
+
+
+
+    return 0;
+}
